30f10048927e3e6ba8c7e050e51402813d943ddf
dulleo <dulleo@gmail.com>
2019-01-15 00:17:42 +0100

    getAllQuestion method added

--- src/main/java/com/duskol/ecdlv2/service/QuestionService.java
+++ src/main/java/com/duskol/ecdlv2/service/QuestionService.java
@@ -1,10 +1,16 @@
 package com.duskol.ecdlv2.service;
 
+import java.util.List;
+
+import com.duskol.ecdlv2.dto.QuestionDTO;
+
 /**
  * 
  * Cretaed Jan 15, 2019 by duskol
  *
  */
 public interface QuestionService {
+
+	List<QuestionDTO> getAllQuestions(Long testId);
 
 }
