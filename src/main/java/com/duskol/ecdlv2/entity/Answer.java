a4e999d5edd1bfc568e14af1539bb0125f1ca3ae
dulleo <dulleo@gmail.com>
2019-01-14 13:10:17 -0700

    properties added

--- src/main/java/com/duskol/ecdlv2/entity/Answer.java
+++ src/main/java/com/duskol/ecdlv2/entity/Answer.java
@@ -1,10 +1,61 @@
 package com.duskol.ecdlv2.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  * 
  * Created by Dusko Lucic on Jan 14, 2019
  *
  */
+@Entity
+@Table(name="answers")
 public class Answer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="correct")
+	private Boolean isCorrect;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "question_id", nullable = false)
+	private Question question;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Boolean getIsCorrect() {
+		return isCorrect;
+	}
+
+	public void setIsCorrect(Boolean isCorrect) {
+		this.isCorrect = isCorrect;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	@Override
+	public String toString() {
+		return "Answer [id=" + id + ", isCorrect=" + isCorrect + ", question=" + question + "]";
+	}
 }
