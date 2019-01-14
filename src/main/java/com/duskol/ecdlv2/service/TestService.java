53257e2b9cde20963955c28b4fa97c1ecf4f8147
dulleo <dulleo@gmail.com>
2019-01-14 14:24:17 -0700

    comment added

--- src/main/java/com/duskol/ecdlv2/service/TestService.java
+++ src/main/java/com/duskol/ecdlv2/service/TestService.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 import com.duskol.ecdlv2.dto.TestDTO;
 
+/**
+ * 
+ * Created by Dusko Lucic on Jan 14, 2019
+ *
+ */
 public interface TestService {
 
 	List<TestDTO> getAll();
