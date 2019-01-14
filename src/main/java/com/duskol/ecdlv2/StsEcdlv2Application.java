8c273a8682cf6ff496fc0c82ffc583802d759dbc
dulleo <dulleo@gmail.com>
2019-01-14 11:01:28 -0700

    Initial commit

---
+++ src/main/java/com/duskol/ecdlv2/StsEcdlv2Application.java
@@ -0,0 +1,14 @@
+package com.duskol.ecdlv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsEcdlv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsEcdlv2Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/duskol/ecdlv2/StsEcdlv2ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.duskol.ecdlv2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StsEcdlv2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
