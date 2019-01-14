c446af53df13a32e639bc4d36a5d49b5c227d732
dulleo <dulleo@gmail.com>
2019-01-14 11:49:56 -0700

    properties added

---
+++ src/main/java/com/duskol/ecdlv2/entity/Question.java
@@ -0,0 +1,82 @@
+package com.duskol.ecdlv2.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import com.duskol.ecdlv2.common.QuestionType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+/**
+ * 
+ * Created by Dusko Lucic on Jan 14, 2019
+ *
+ */
+@Entity
+@Table(name="questions")
+public class Question {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Column(name="text")
+	private String text;
+	
+	@Column(name="type")
+	@Enumerated(EnumType.STRING)
+	private QuestionType type;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "test_id", nullable = false)
+	//@OnDelete(action = OnDeleteAction.CASCADE) da li ovo koristiti????
+	@JsonIgnore 
+	private Test test;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public QuestionType getType() {
+		return type;
+	}
+
+	public void setType(QuestionType type) {
+		this.type = type;
+	}
+
+	public Test getTest() {
+		return test;
+	}
+
+	public void setTest(Test test) {
+		this.test = test;
+	}
+
+	@Override
+	public String toString() {
+		return "Question [id=" + id + ", text=" + text + ", type=" + type + ", test=" + test + "]";
+	}
+}
