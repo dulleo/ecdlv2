54e1c049e1341d8826e946dd3a709f4eeab250d9
dulleo <dulleo@gmail.com>
2019-01-14 11:35:51 -0700

    properties added

--- src/main/java/com/duskol/ecdlv2/entity/Test.java
+++ src/main/java/com/duskol/ecdlv2/entity/Test.java
@@ -1,5 +1,48 @@
 package com.duskol.ecdlv2.entity;
 
-public class Test {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
+/**
+ * 
+ * Created by Dusko Lucic on Jan 14, 2019
+ *
+ */
+@Entity
+@Table(name="tests")
+public class Test {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Column(name="title")
+	private String title;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String toString() {
+		return "Test [id=" + id + ", title=" + title + "]";
+	}
 }
