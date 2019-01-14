0bdfb43db27aa467a2edca2b9fc9d9d158f62b40
dulleo <dulleo@gmail.com>
2019-01-15 00:35:11 +0100

    ResourceNotFoundException added for getAllQuestions

--- src/main/java/com/duskol/ecdlv2/controller/QuestionController.java
+++ src/main/java/com/duskol/ecdlv2/controller/QuestionController.java
@@ -1,7 +1,16 @@
 package com.duskol.ecdlv2.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.duskol.ecdlv2.dto.QuestionDTO;
+import com.duskol.ecdlv2.exception.ResourceNotFoundException;
+import com.duskol.ecdlv2.service.QuestionService;
 
 /**
  * 
@@ -11,5 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/ecdl/tests")
 public class QuestionController {
+	
+	@Autowired
+	private QuestionService questionService;
+	
+	@GetMapping("/{testId}/questions")
+	public List<QuestionDTO> getAllQuestions(@PathVariable Long testId) throws ResourceNotFoundException {
+		return questionService.getAllQuestions(testId);
+	}
 
 }
