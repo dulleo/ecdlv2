5e26edb5894efda8ffb70f53865ce6bfc1a6556f
dulleo <dulleo@gmail.com>
2019-01-14 13:28:24 -0700

    getTests added

--- src/main/java/com/duskol/ecdlv2/controller/TestController.java
+++ src/main/java/com/duskol/ecdlv2/controller/TestController.java
@@ -1,10 +1,31 @@
 package com.duskol.ecdlv2.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.duskol.ecdlv2.dto.TestDTO;
+import com.duskol.ecdlv2.service.TestService;
+
 /**
  * 
  * Created by Dusko Lucic on Jan 14, 2019
  *
  */
+@RestController
+@RequestMapping(value="/ecdlv2")
 public class TestController {
+	
+	@Autowired
+	private TestService testService;
+	
+	@GetMapping("/tests")
+	public List<TestDTO> getAllTests() {
+		return testService.getTests();
+	}
 
+	
 }
