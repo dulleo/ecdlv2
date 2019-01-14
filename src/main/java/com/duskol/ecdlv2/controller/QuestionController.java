8ce9612882a017bb19d76732e245d31597e87014
dulleo <dulleo@gmail.com>
2019-01-15 00:50:04 +0100

    createQuestion added

--- src/main/java/com/duskol/ecdlv2/controller/QuestionController.java
+++ src/main/java/com/duskol/ecdlv2/controller/QuestionController.java
@@ -2,9 +2,13 @@ package com.duskol.ecdlv2.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +31,11 @@ public class QuestionController {
 	@GetMapping("/{testId}/questions")
 	public List<QuestionDTO> getAllQuestions(@PathVariable Long testId) throws ResourceNotFoundException {
 		return questionService.getAllQuestions(testId);
+	}
+	
+	@PostMapping("/{testId}/questions")
+	public void createQuestion(@PathVariable Long testId, @Valid @RequestBody QuestionDTO questionDTO) throws ResourceNotFoundException {
+		questionService.createQuestion(testId, questionDTO);
 	}
 
 }
