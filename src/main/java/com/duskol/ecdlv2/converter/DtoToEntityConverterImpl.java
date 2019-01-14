12d9b53c5bae70e20227110a7f30e4d019b98bf0
dulleo <dulleo@gmail.com>
2019-01-14 14:21:21 -0700

    initial commit

---
+++ src/main/java/com/duskol/ecdlv2/converter/DtoToEntityConverterImpl.java
@@ -0,0 +1,22 @@
+package com.duskol.ecdlv2.converter;
+
+import org.springframework.stereotype.Component;
+
+import com.duskol.ecdlv2.dto.TestDTO;
+import com.duskol.ecdlv2.entity.Test;
+
+/**
+ * 
+ * Created by Dusko Lucic on Jan 14, 2019
+ *
+ */
+@Component
+public class DtoToEntityConverterImpl implements DtoToEntityConverter {
+
+	@Override
+	public void convert(TestDTO testDTO, Test test) {
+		
+		
+	}
+
+}
