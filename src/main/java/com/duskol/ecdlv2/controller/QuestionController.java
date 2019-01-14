6c8ae4fa2211f214f56b3d894022bd5dffd8b939
dulleo <dulleo@gmail.com>
2019-01-15 00:02:25 +0100

    initial commit

---
+++ src/main/java/com/duskol/ecdlv2/controller/QuestionController.java
@@ -0,0 +1,15 @@
+package com.duskol.ecdlv2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * Cretaed Jan 15, 2019 by duskol
+ *
+ */
+@RestController
+@RequestMapping(value="/ecdl/tests")
+public class QuestionController {
+
+}
