16307472b709264083e1e350d49bc1f772a900ec
dulleo <dulleo@gmail.com>
2019-01-15 01:35:20 +0100

    ResourceNotFoundException for update added

--- src/main/java/com/duskol/ecdlv2/service/QuestionService.java
+++ src/main/java/com/duskol/ecdlv2/service/QuestionService.java
@@ -16,6 +16,6 @@ public interface QuestionService {
 
 	void create(Long testId, QuestionDTO questionDTO) throws ResourceNotFoundException;
 
-	void update(Long testId, Long questionId, QuestionDTO questionDTO);
+	void update(Long testId, Long questionId, QuestionDTO questionDTO) throws ResourceNotFoundException;
 
 }
