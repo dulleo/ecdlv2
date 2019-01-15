e61e94942132d6d727948ce6411c0240d5544209
dulleo <dulleo@gmail.com>
2019-01-15 01:15:03 +0100

    deleted

--- src/main/java/com/duskol/ecdlv2/repository/AnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.duskol.ecdlv2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.duskol.ecdlv2.entity.Answer;
-
-public interface AnswerRepository extends JpaRepository<Answer, Long>{
-
-}
