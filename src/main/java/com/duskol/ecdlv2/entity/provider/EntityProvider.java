019e6b32c9c909f52d326de2c2894552bf38e142
dulleo <dulleo@gmail.com>
2019-01-19 11:10:30 +0100

    implementation

---
+++ src/main/java/com/duskol/ecdlv2/entity/provider/EntityProvider.java
@@ -0,0 +1,52 @@
+package com.duskol.ecdlv2.entity.provider;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.duskol.ecdlv2.entity.Question;
+import com.duskol.ecdlv2.entity.Test;
+import com.duskol.ecdlv2.exception.ResourceNotFoundException;
+import com.duskol.ecdlv2.repository.RepositoryContainer;
+
+/**
+ * 
+ * Cretaed Jan 19, 2019 by duskol
+ *
+ */
+@Component
+public class EntityProvider implements EntityProviderInterface {
+
+	@Autowired
+	private RepositoryContainer repositoryContainer;
+
+	/**
+	 * 
+	 * @param testId
+	 * @return
+	 * @throws ResourceNotFoundException
+	 */
+	@Override
+	public Test getTest(Long testId) throws ResourceNotFoundException {
+
+		Optional<Test> testOptional = repositoryContainer.getTestRepository().findById(testId);
+
+		if (!testOptional.isPresent()) {
+			throw new ResourceNotFoundException("Test id: " + testId + " not found!");
+		}
+		return testOptional.get();
+	}
+
+	@Override
+	public Question getQuestion(Long testId, Long questionId) throws ResourceNotFoundException {
+
+		Question question = repositoryContainer.getQuestionRepository().findByIdAndTestId(testId, questionId);
+
+		if (question == null) {
+			throw new ResourceNotFoundException("Question not found with id " + questionId + " and testId " + testId);
+		}
+		return null;
+	}
+
+}
