bcc10037ebe9525ed63695cfe828ce35f6f388bb
dulleo <dulleo@gmail.com>
2019-02-17 13:02:45 +0100

    DtoToEntityConverter implementation

--- src/main/java/com/duskol/ecdlv2/converter/DtoToEntityConverter.java
+++ src/main/java/com/duskol/ecdlv2/converter/DtoToEntityConverter.java
@@ -1,5 +1,7 @@
 package com.duskol.ecdlv2.converter;
 
+import org.springframework.stereotype.Component;
+
 import com.duskol.ecdlv2.dto.AnswerDTO;
 import com.duskol.ecdlv2.dto.QuestionDTO;
 import com.duskol.ecdlv2.dto.TestDTO;
@@ -12,11 +14,31 @@ import com.duskol.ecdlv2.entity.Test;
  * Created by Dusko Lucic on Jan 14, 2019
  *
  */
-public interface DtoToEntityConverter {
+@Component
+public class DtoToEntityConverter implements DtoToEntityConverterInterface {
 
-	void convert(TestDTO testDTO, Test test);
+	@Override
+	public void convert(TestDTO testDTO, Test test) {
+		test.setTitle(testDTO.getTitle());
+		test.setDuration(testDTO.getDuration());
+		test.setPassingLimit(testDTO.getPassingLimit());
+		test.setStatus(testDTO.getStatus());
+		test.setTotalExamQuestions(testDTO.getTotalExamQuestions());
+	}
 
-	void convert(QuestionDTO questionDTO, Question question);
+	@Override
+	public void convert(QuestionDTO questionDTO, Question question) {
+		question.setText(questionDTO.getText());
+		question.setType(questionDTO.getType());
+	}
 
-	void convert(AnswerDTO answerDTO, Answer answer);
+	@Override
+	public void convert(AnswerDTO answerDTO, Answer answer) {
+		if(answerDTO.getId() != null) {
+			answer.setId(answerDTO.getId());
+		}
+		answer.setIsCorrect(answerDTO.getIsCorrect());
+		answer.setText(answerDTO.getText());
+	}
+
 }
