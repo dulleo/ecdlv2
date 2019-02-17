5b14ee337bb75636250baa3859572c14c039ba82
dulleo <dulleo@gmail.com>
2019-02-17 13:15:01 +0100

    properties added

--- src/main/java/com/duskol/ecdlv2/entity/Test.java
+++ src/main/java/com/duskol/ecdlv2/entity/Test.java
@@ -1,12 +1,18 @@
 package com.duskol.ecdlv2.entity;
 
+import java.math.BigDecimal;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+
+import com.duskol.ecdlv2.common.TestStatus;
 
 /**
  * 
@@ -24,6 +30,19 @@ public class Test {
 	@NotBlank
 	@Column(name="title")
 	private String title;
+	
+	@Column(name="duration")
+	private Integer duration;
+	
+	@Column(name="passing_limit")
+	private BigDecimal passingLimit;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name="status")
+	private TestStatus status;
+	
+	@Column(name="total_exam_questions")
+	private Integer totalExamQuestions;
 
 	public Long getId() {
 		return id;
@@ -41,8 +60,35 @@ public class Test {
 		this.title = title;
 	}
 
-	@Override
-	public String toString() {
-		return "Test [id=" + id + ", title=" + title + "]";
+	public Integer getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
+
+	public BigDecimal getPassingLimit() {
+		return passingLimit;
+	}
+
+	public void setPassingLimit(BigDecimal passingLimit) {
+		this.passingLimit = passingLimit;
+	}
+
+	public TestStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(TestStatus status) {
+		this.status = status;
+	}
+
+	public Integer getTotalExamQuestions() {
+		return totalExamQuestions;
+	}
+
+	public void setTotalExamQuestions(Integer totalExamQuestions) {
+		this.totalExamQuestions = totalExamQuestions;
 	}
 }
