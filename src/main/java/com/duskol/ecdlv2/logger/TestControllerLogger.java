a0740df05f88bae712c407f38287062a49d60879
dulleo <dulleo@gmail.com>
2019-02-19 12:18:16 +0100

    @Before removed

--- src/main/java/com/duskol/ecdlv2/logger/TestControllerLogger.java
+++ src/main/java/com/duskol/ecdlv2/logger/TestControllerLogger.java
@@ -1,30 +1,140 @@
 package com.duskol.ecdlv2.logger;
 
-import java.util.Arrays;
+import java.util.List;
 
-import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.hibernate.JDBCException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.duskol.ecdlv2.dto.TestDTO;
+import com.duskol.ecdlv2.error.ErrorCodes;
+import com.duskol.ecdlv2.exception.DataIntegrityException;
+import com.duskol.ecdlv2.exception.InternalException;
+import com.duskol.ecdlv2.exception.NotFoundException;
+import com.duskol.ecdlv2.exception.ResourceNotFoundException;
 
 @Aspect
 @Component
 public class TestControllerLogger {
 	
-	private final static Logger LOGGER = LoggerFactory.getLogger(TestControllerLogger.class);
-	private static String METHOD_NAME;
-	private static final String MESSAGE_FORMAT_START = "Method \"{}\" has been started";
+	@Autowired
+	private CommonLogger commonLogger;
 	
-	@Before("@annotation(org.springframework.web.bind.annotation.RequestMapping) && @annotation(mapping)")
-	public void advice(JoinPoint jp, RequestMapping mapping) {
-		METHOD_NAME = jp.getSignature().getName();
-		LOGGER.info(MESSAGE_FORMAT_START, METHOD_NAME);
-		LOGGER.info("Method Type: {}", mapping.method()[0]);
-		LOGGER.info("URL: {}", mapping.value()[0]);
-		LOGGER.info("Args: {}",Arrays.toString(jp.getArgs()));
-	}
+	private final static Logger LOGGER = LoggerFactory.getLogger(TestControllerLogger.class);
+	private static final String MESSAGE_FORMAT_ERROR = "Method \"{}\" unsuccessfully finished. ERROR: {}";
+	
+	@Pointcut("execution(java.util.List<com.duskol.ecdlv2.dto.TestDTO> com.duskol.ecdlv2.controller.TestController.getAllTests())")
+	public void getAllTestPointcut() { }
+	
+	@SuppressWarnings("unchecked")
+	@Around("getAllTestPointcut()")
+	public List<TestDTO> getTest(ProceedingJoinPoint jp) throws Throwable {
 
+		List<TestDTO> result = null;
+		
+		try {
+			result = (List<TestDTO>) jp.proceed();
+			LOGGER.info("Result: " + result.toString());
+		} catch (Exception e) {
+			getInternalError(e, ErrorCodes.TESTS_CAN_NOT_BE_PROVIDED, commonLogger.getMethodName());
+		}
+		return result;
+	}
+	
+	@Pointcut("execution(com.duskol.ecdlv2.dto.TestDTO com.duskol.ecdlv2.controller.TestController.getTest(Long)) "
+			+ "&& args(id)")
+	public void getTestPointcut(Long id) { }
+	
+	@Around("getTestPointcut(id)")
+	public TestDTO getTest(ProceedingJoinPoint jp, Long id) throws Throwable {
+
+		TestDTO result = null;
+		
+		try {
+			result = (TestDTO) jp.proceed();
+			LOGGER.info("Result: " + result.toString());
+		} catch (ResourceNotFoundException e) {
+			getNotFoundError(e, ErrorCodes.TEST_CAN_NOT_BE_PROVIDED, commonLogger.getMethodName());
+		} catch (DataIntegrityViolationException e) {
+			getDataIntegrityViolationError(e, ErrorCodes.TEST_CAN_NOT_BE_PROVIDED, commonLogger.getMethodName());
+		} catch (DataAccessException e) {
+			getDataAccessError(e, ErrorCodes.TEST_CAN_NOT_BE_PROVIDED, commonLogger.getMethodName());
+		} catch (Exception e) {
+			getInternalError(e, ErrorCodes.TEST_CAN_NOT_BE_PROVIDED, commonLogger.getMethodName());
+		}
+		return result;
+	}
+	
+	/**
+	 * 
+	 * @param e
+	 * @param errorCodes
+	 * @param methodName
+	 * @throws NotFoundException
+	 */
+	private void getNotFoundError(Exception e, ErrorCodes errorCodes, String methodName) throws NotFoundException {
+		LOGGER.error(MESSAGE_FORMAT_ERROR, methodName, e.getMessage(),e);
+		throw new NotFoundException(e.getMessage(), errorCodes);
+	}
+	
+	/**
+	 * 
+	 * @param e
+	 * @param errorCodes
+	 * @param methodName
+	 * @throws DataIntegrityException
+	 */
+	private void getDataIntegrityViolationError(DataIntegrityViolationException e, ErrorCodes errorCodes, String methodName) throws DataIntegrityException {
+		generateError(e, errorCodes, methodName);    
+	}
+	
+	/**
+	 * 
+	 * @param e
+	 * @param errorCodes
+	 * @param methodName
+	 * @throws DataIntegrityException
+	 */
+	private void getDataAccessError(DataAccessException e, ErrorCodes errorCodes, String methodName) throws DataIntegrityException {
+		generateError(e, errorCodes, methodName); 
+	}
+	
+	/**
+	 * 
+	 * @param e
+	 * @param errorCodes
+	 * @param methodName
+	 * @throws DataIntegrityException
+	 */
+	private void generateError(Exception e, ErrorCodes errorCodes, String methodName) throws DataIntegrityException {
+		if(e.getCause() instanceof JDBCException && ((JDBCException)e.getCause()).getSQLException() != null)
+        {                    
+			String message = ((JDBCException)e.getCause()).getSQLException().getMessage();
+			LOGGER.error(MESSAGE_FORMAT_ERROR,methodName,message,e);
+            throw new DataIntegrityException(message, errorCodes);
+        } else {
+        	LOGGER.error(MESSAGE_FORMAT_ERROR,methodName,e.getMessage(), e);
+            throw new DataIntegrityException(e.getMessage(), errorCodes); 
+        }
+	}
+	
+	/**
+	 * 
+	 * @param e
+	 * @param errorCodes
+	 * @param methodName
+	 * @throws InternalException
+	 */
+	private void getInternalError(Exception e, ErrorCodes errorCodes, String methodName) throws InternalException {
+		LOGGER.error(MESSAGE_FORMAT_ERROR, methodName, e.getMessage(), e);
+		throw new InternalException(e.getMessage(), errorCodes);
+	}
 }
