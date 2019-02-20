d3b7306468a117aa97edd8cbd3749dcd7ecb407d
dulleo <dulleo@gmail.com>
2019-02-21 00:48:52 +0100

    getTest method added

--- src/main/java/com/duskol/ecdlv2/entity/provider/EntityProviderInterface.java
+++ src/main/java/com/duskol/ecdlv2/entity/provider/EntityProviderInterface.java
@@ -1,7 +1,9 @@
 package com.duskol.ecdlv2.entity.provider;
 
+import com.duskol.ecdlv2.dto.TestDTO;
 import com.duskol.ecdlv2.entity.Question;
 import com.duskol.ecdlv2.entity.Test;
+import com.duskol.ecdlv2.exception.EntityValidationException;
 import com.duskol.ecdlv2.exception.ResourceNotFoundException;
 
 /**
@@ -14,5 +16,7 @@ public interface EntityProviderInterface {
 	Test getTest(Long testId) throws ResourceNotFoundException;
 	
 	Question getQuestion(Long testId, Long questionId) throws ResourceNotFoundException;
+
+	Test getTest(Long testId, TestDTO testDTO) throws EntityValidationException, ResourceNotFoundException;
 
 }
