36a5988eef5759cce2d3542befd6cf0d855ebb46
dulleo <dulleo@gmail.com>
2019-02-21 00:48:15 +0100

    class added

---
+++ src/main/java/com/duskol/ecdlv2/exception/EntityValidationException.java
@@ -0,0 +1,18 @@
+package com.duskol.ecdlv2.exception;
+
+/**
+ * 
+ * Cretaed Feb 21, 2019 by duskol
+ *
+ */
+public class EntityValidationException extends Exception {
+
+	private static final long serialVersionUID = 9118447552278828722L;
+
+	public EntityValidationException() {
+	}
+
+	public EntityValidationException(String message) {
+		super(message);
+	}	
+}
