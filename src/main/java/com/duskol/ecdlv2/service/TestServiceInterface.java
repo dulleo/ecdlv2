71dfbe44c3ed8d6c2f7ac8ff4358cbc77b6c240c
dulleo <dulleo@gmail.com>
2019-02-21 00:45:25 +0100

    methods renamed and exception added

--- src/main/java/com/duskol/ecdlv2/service/TestServiceInterface.java
+++ src/main/java/com/duskol/ecdlv2/service/TestServiceInterface.java
@@ -3,6 +3,7 @@ package com.duskol.ecdlv2.service;
 import java.util.List;
 
 import com.duskol.ecdlv2.dto.TestDTO;
+import com.duskol.ecdlv2.exception.EntityValidationException;
 import com.duskol.ecdlv2.exception.ResourceNotFoundException;
 
 /**
@@ -14,11 +15,11 @@ public interface TestServiceInterface {
 
 	List<TestDTO> getAll();
 
-	void save(TestDTO testDTO);
+	void createTest(TestDTO testDTO);
 
-	void update(Long testId, TestDTO testDTO) throws ResourceNotFoundException;
+	void updateTest(Long testId, TestDTO testDTO) throws ResourceNotFoundException, EntityValidationException;
 
-	void delete(Long testId) throws ResourceNotFoundException;
+	void deleteTest(Long testId) throws ResourceNotFoundException;
 
 	TestDTO getTest(Long id) throws ResourceNotFoundException;
 
